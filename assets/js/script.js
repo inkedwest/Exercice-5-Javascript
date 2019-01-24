d511e1c7a05803e6a5499ccd50f42408143bc438
Glatigny <f.west@live.fr>
2019-01-24 18:42:30 +0100

    Correction exo 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
-// Déclaration de la fonction
+// déclaration de la fonction
 function multiplyReal() {
       var firstNumber = document.getElementById('firstNumber').value; //type string
       var secondNumber = document.getElementById('secondNumber').value; // type string
       //isNan(parameter) is Not a Number
-      // ! NOT
+      // ! NOT//Sinon
+      // Condition
       if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
-        if (Number(firstNumber) % 1 !== 0 && Number(secondNumber) % 1 !== 0) { // % s'appelle un modulo ; on s'en sert pour déterminer si le nombre est à virgule. si le reste est différent de 0 alors c'est un nombre invariable
+        // On se sert du modulo pour déterminé si le nombre est à virgule. si le reste est différant de 0 alors c'est un nombre à virgule
+        if (Number(firstNumber) % 1 !== 0 && Number(secondNumber) % 1 !== 0) {
           firstNumberInteger = Math.trunc(firstNumber);
           result = firstNumberInteger * secondNumber;
           alert(result);
+          
         } else {
           alert('ce sont des nombres entiers');
         }
