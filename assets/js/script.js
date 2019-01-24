efd295554c035afb602b73158ca1e780b1a5a678
Glatigny <f.west@live.fr>
2019-01-24 13:52:17 +0100

    correction exo 5 js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
-function multiplyReel() {
+// Déclaration de la fonction
+function multiplyReal() {
       var firstNumber = document.getElementById('firstNumber').value; //type string
       var secondNumber = document.getElementById('secondNumber').value; // type string
       //isNan(parameter) is Not a Number
       // ! NOT
       if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
-        if (Number(firstNumber) % 1 !== 0 && Number(secondNumber) % 1 !== 0) {
-          a1 = Math.trunc(firstNumber);
-          a2 = secondNumber;
-          result = a1 * a2;
+        if (Number(firstNumber) % 1 !== 0 && Number(secondNumber) % 1 !== 0) { // % s'appelle un modulo ; on s'en sert pour déterminer si le nombre est à virgule. si le reste est différent de 0 alors c'est un nombre invariable
+          firstNumberInteger = Math.trunc(firstNumber);
+          result = firstNumberInteger * secondNumber;
           alert(result);
         } else {
-          alert("ce ne sont pas des nombre entiers");
+          alert('ce sont des nombres entiers');
         }
       } else {
-        alert("attention ce ne sont pas des nombres");
+        alert('attention ce ne sont pas des nombres');
       }
     }
